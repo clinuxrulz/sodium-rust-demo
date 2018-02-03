0dc4bc982f8cbf0af96b78c730cfa00a79399f34
Clinton Selke <clinuxrulz@gmail.com>
2018-02-03 22:43:51 +1000

    JS code executed from Rust (circle added to SVG test)

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,6 +10,11 @@ mod app_ctx;
 
 use app_ctx::AppCtx;
 
+extern {
+    fn display_add_circle(x: u32, y: u32, r: u32) -> u32;
+    fn display_remove(id: u32);
+}
+
 #[no_mangle]
 pub extern "C" fn create_app_ctx() -> *mut AppCtx {
     Box::into_raw(Box::new(AppCtx::new()))
@@ -18,4 +23,11 @@ pub extern "C" fn create_app_ctx() -> *mut AppCtx {
 #[no_mangle]
 pub extern "C" fn destroy_app_ctx(app_ctx: *mut AppCtx) {
     unsafe { Box::from_raw(app_ctx) };
+}
+
+#[no_mangle]
+pub extern "C" fn test() {
+    unsafe {
+        display_add_circle(50, 50, 10);
+    }
 }
